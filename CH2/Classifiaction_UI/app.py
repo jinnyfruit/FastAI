add693761a4e02ded96117019e974f84c49c0b99
jinnyfruit <68141544+jinnyfruit@users.noreply.github.com>
2023-01-17 14:11:26 -0800

    Cat vs Dog classificationUI using Huggingface

--- CH2/Classifiaction_UI/app.py
+++ CH2/Classifiaction_UI/app.py
@@ -1,7 +1,19 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
+def is_cat(x): return x[0].isupper()
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+categories = ("Dog","Cat")
+
+learn = load_learner("model.pkl")
+
+def classify_image(img):
+  pred, idx, probs = learn.predict(img)
+  return dict(zip(categories, map(float,probs)))
+
+image = gr.inputs.Image(shape=(200,200))
+label = gr.outputs.Label()
+examples = ['dog.jpeg', 'cat.jpeg', 'jinny.jpeg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
+intf.launch()
